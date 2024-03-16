c891f3429546dde68b3d30d9af606555d9154489
IvanaJindrova <91778575+IvanaJindrova@users.noreply.github.com>
2024-03-16 22:47:07 +0100

    Úprava dovedností

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
 console.log('funguju!');
+/*
+//Jednotlivé dovednosti zvlášť
 
-const zadanaUrovenHTML = Number(Math.round(prompt("Zadej úrovni svých dovední v HTML na škále od 0 - 100")))
+const zadanaUrovenHTML = Number(Math.round(prompt("Zadej úrovni svých dovedností v HTML na škále od 0 - 100")))
 console.log(zadanaUrovenHTML)
 
-const HTMLHodnota = document.getElementById('skill1') // - najdu konkrétní element dle ID
-const HTMLValue = document.querySelector('#skill__value') //tady chci najít element pod ID
-const HTMLtext = HTMLValue.textContent
-HTMLValue.textContent = zadanaUrovenHTML 
+const HTMLHodnota = document.getElementById('skill1') 
+const HTMLValue = document.querySelector('.skill__value') 
+HTMLValue.textContent = zadanaUrovenHTML + " / 100"
+const HTMLBar = document.querySelector('.skill__progress')
+HTMLBar.style.width = zadanaUrovenHTML + "%"
 
 
+const zadanaUrovenCSS = Number(Math.round(prompt("Zadej úrovni svých dovedostí v CSS na škále od 0 - 100")))
+console.log(zadanaUrovenCSS)
+
+const CSSHodnota = document.getElementById('skill2') 
+const CSSValue = document.querySelector('.skill__value') 
+CSSValue.textContent = zadanaUrovenCSS + " / 100"
+const CSSBar = document.querySelector('.skill__bar')
+const CSSBarr = document.querySelector('.skill__progress')
+CSSBarr.style.width = zadanaUrovenCSS + "%"
 
 
+const zadanaUrovenJS = Number(Math.round(prompt("Zadej úrovni svých dovedností v JavaScriptu na škále od 0 - 100"))) 
+console.log(zadanaUrovenJS)
 
-
-
-/* -> OSTATNÍ CSS + JS
-const zadanaUrovenCSS = Number(prompt("Zadej úrovni svých dovední v CSS na škále od 0 - 100"))
-const zadanaUrovenJS = Number(prompt("Zadej úrovni svých dovední v JS na škále od 0 - 100"))
+const JSHodnota = document.getElementById('skill3') 
+const JSValue = document.querySelector('.skill__value') 
+JSValue.textContent = zadanaUrovenJS + " / 100" 
+const JSBar = document.querySelector('.skill__progress') 
+JSBar.style.width = zadanaUrovenJS + "%"
 */
+
+//Funkce
+const updateSkill = (idElement, level) => {
+    const hodnota = document.getElementById(idElement) 
+    const value = document.querySelector('.skill__value') 
+    value.textContent = level + " / 100"
+    const bar = document.querySelector('.skill__progress')
+    bar.style.width = level + "%"
+}
+
+updateSkill(skill3, 74)
